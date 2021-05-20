99ad0eb6adcf9160009cefd201aeb5215261ee87
morningred88 <xma50089@gmail.com>
2021-05-20 18:45:58 +0000

    Changed the team name and added gitignore

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Your Team Name';
-    version = '1.0.0';
+    team = 'Morningred';
+    version = '1.0.1';
 }
 module.exports = AppConfig;
